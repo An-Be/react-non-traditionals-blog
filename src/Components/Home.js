63993a38c6297ad3dbbf8d6f92d90e31be16ca97
Andrea <andrea.berrocal95@gmail.com>
2022-05-14 15:14:54 -0700

    ⚡️ changed layout of home page, moved some cards to about page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,57 +1,58 @@
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import question from '../Images/question.png';
-import award from '../Images/award.png';
-import start from '../Images/start.png';
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import register from "../Images/register.png";
+import getStarted from "../Images/getStarted.png";
 
-
-
-
-function Home(){
-    return(
-        <>
-        <div className="container" id="intro">
-            <div className="row">
-                <div className="col-md">
-                    <h1>Welcome to Non-Traditionals Blog &#128075;</h1>
-                    <p>LOREM IPSUM</p>
-                </div>
-            </div>                            
+function Home() {
+  return (
+    <>
+      <div className="container" id="intro">
+        <div className="row">
+          <div className="col-md">
+            <h1>Welcome to Non-Traditionals Blog &#128075;</h1>
+            <p>
+              This is a community for those who are either already in tech or
+              planning to change careers in tech can share their experience and
+              progress.
+            </p>
+          </div>
         </div>
-        <hr/>
+      </div>
+      <div id="getStarted">
         <div className="container">
-            <div className="row">
-                <div className="col-md m-1">
-                    <Card className="Card">
-                       <CardImg className="CardImg" src={question} ></CardImg>                    
-                       <CardBody>
-                           <CardTitle>What is this place?</CardTitle>
-                           <CardText>LOREM IPSUM</CardText>
-                       </CardBody>
-                    </Card>
-                </div>
-                <div className="col-md m-1">
-                    <Card className="Card">
-                       <CardImg className="CardImg" src={award}></CardImg> 
-                       <CardBody>
-                           <CardTitle>I have a degree can I join?</CardTitle>
-                           <CardText>LOREM IPSUM</CardText>
-                       </CardBody>
-                    </Card>
-                </div>
-                <div className="col-md m-1">
-                    <Card className="Card">
-                       <CardImg className="CardImg" src={start}></CardImg> 
-                       <CardBody>
-                           <CardTitle>How do I start?</CardTitle>
-                           <CardText>LOREM IPSUM</CardText>
-                       </CardBody>
-                    </Card>
-                </div>
+          <div className="row">
+            <div className="col-md col-6" id="getStartedText">
+              <p>
+                To get the best experience at Non-traditionals making an account
+                is recommended but if you are just here to browse blogs all you
+                have to do is click on Blogs at the top of the page.
+              </p>
             </div>
+            <div className="col-md col-2">
+              <img
+                src={getStarted}
+                width="250"
+                alt="logo for getting started"
+              ></img>
+            </div>
+          </div>
         </div>
-        <hr />
-        </>
-    );
+      </div>
+      <hr />
+      <div className="container">
+        <div className="row">
+          <div className="col-md m-1">
+            <Card className="Card">
+              <CardImg className="CardImg" src={register} width="100"></CardImg>
+              <CardBody>
+                <CardTitle>Register and make a profile</CardTitle>
+                <CardText>LOREM IPSUM</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
